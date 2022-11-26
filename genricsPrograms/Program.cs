576acb89e296ee8b0ac34d479df2a873b40b12e7
AnilBamne <anilbamne20@gmail.com>
2022-11-26 22:11:12 +0530

    Adding welcome message

---
+++ genricsPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace genricsPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Genrics Programs");
+        }
+    }
+}
