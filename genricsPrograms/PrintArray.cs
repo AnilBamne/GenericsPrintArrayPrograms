a25544d9c3542874b91d7523d3a67b705136af15
AnilBamne <anilbamne20@gmail.com>
2022-11-26 22:15:23 +0530

    Adding UC1PrintThreeArrays code

---
+++ genricsPrograms/PrintArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace genricsPrograms
+{
+    public class PrintArray
+    {
+        //method prints array of integers
+        public static void toPrint(int[] inputArray)
+        {
+            foreach (int element in inputArray) // 1, 2, 3, 4, 5
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine("\n-----------");
+        }
+        //method prints array of double
+
+        public static void toPrint(double[] inputArray)
+        {
+            foreach (double element in inputArray)//1.1, 2.2, 3.3, 4.4
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine("\n-----------");
+        }
+        //method prints array of charecters
+
+        public static void toPrint(char[] inputArray)
+        {
+            foreach (char element in inputArray) //'H', 'E', 'L', 'L', 'O'
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine("\n-----------");
+        }
+    }
+}
--- genricsPrograms/Program.cs
+++ genricsPrograms/Program.cs
@@ -7,6 +7,12 @@ namespace genricsPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Genrics Programs");
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+            PrintArray.toPrint(intArray);
+            PrintArray.toPrint(doubleArray);
+            PrintArray.toPrint(charArray);
         }
     }
 }
